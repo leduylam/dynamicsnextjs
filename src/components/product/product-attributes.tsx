9102f1d0a1107564da99f4c2176d62100d92351f
leduylam <leduylam1991@gmail.com>
2025-02-14 10:48:00 +0700

    update product attribute width

--- src/components/product/product-attributes.tsx
+++ src/components/product/product-attributes.tsx
@@ -59,7 +59,7 @@ export const ProductAttributes: React.FC<Props> = ({
                     <li
                       key={`${value}-${id}`}
                       className={cn(
-                        'cursor-pointer rounded border w-9 md:min-w-11 h-9 md:h-11 p-1 mb-2 md:mb-3 ltr:mr-2 rtl:ml-2 ltr:md:mr-3 rtl:md:ml-3 flex justify-center items-center text-heading text-xs md:text-sm uppercase font-semibold transition duration-200 ease-in-out hover:border-black',
+                        'cursor-pointer rounded border w-9 md:min-w-11 md:w-auto h-9 md:h-11 p-1 mb-2 md:mb-3 ltr:mr-2 rtl:ml-2 ltr:md:mr-3 rtl:md:ml-3 flex justify-center items-center text-heading text-xs md:text-sm uppercase font-semibold transition duration-200 ease-in-out hover:border-black',
                         id === defuatlActive ? 'border-black' : 'border-gray-100'
                       )}
                       onClick={() => handleAttributeParent({ [title]: value }, id)}
@@ -80,7 +80,7 @@ export const ProductAttributes: React.FC<Props> = ({
                       <li
                         key={`${value}-${id}`}
                         className={cn(
-                          'cursor-pointer rounded border min-w-9 md:min-w-11 h-9 md:h-11 p-1 mb-2 md:mb-3 ltr:mr-2 rtl:ml-2 ltr:md:mr-3 rtl:md:ml-3 flex justify-center items-center text-heading text-xs md:text-sm font-semibold transition duration-200 ease-in-out hover:border-black',
+                          'cursor-pointer rounded border min-w-9 md:min-w-11 md:w-auto h-9 md:h-11 p-1 mb-2 md:mb-3 ltr:mr-2 rtl:ml-2 ltr:md:mr-3 rtl:md:ml-3 flex justify-center items-center text-heading text-xs md:text-sm font-semibold transition duration-200 ease-in-out hover:border-black',
                           id === subActive ? 'border-black' : 'border-gray-100'
                         )}
                         onClick={() => handleAttributeChildren({ [title]: value }, id)}
@@ -91,7 +91,7 @@ export const ProductAttributes: React.FC<Props> = ({
                       <li
                         key={`${value}-${id}`}
                         className={cn(
-                          'cursor-not-allowed rounded border bg-gray-400 w-9 md:min-w-11 h-9 md:h-11 p-1 mb-2 md:mb-3 ltr:mr-2 rtl:ml-2 ltr:md:mr-3 rtl:md:ml-3 flex justify-center items-center text-heading text-xs md:text-sm font-semibold transition duration-200 ease-in-out',
+                          'cursor-not-allowed rounded border bg-gray-400 w-9 md:min-w-11 md:w-auto h-9 md:h-11 p-1 mb-2 md:mb-3 ltr:mr-2 rtl:ml-2 ltr:md:mr-3 rtl:md:ml-3 flex justify-center items-center text-heading text-xs md:text-sm font-semibold transition duration-200 ease-in-out',
                         )}
                       >
                         {value}
