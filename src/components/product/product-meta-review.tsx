df29e3f1c659565181b2d34c15d8311434b74b99
leduylam <leduylam1991@gmail.com>
2025-08-04 15:59:20 +0700

    change spec

--- src/components/product/product-meta-review.tsx
+++ src/components/product/product-meta-review.tsx
@@ -2,60 +2,56 @@ import { useEffect, useState } from "react";
 import { Collapse } from "@components/common/accordion";
 
 interface Props {
-	data: any;
+  data: any;
 }
 
 const ProductMetaReview: React.FC<Props> = ({ data }) => {
-	const [metas, setMetas] = useState<any[]>([]);
-	const [expanded, setExpanded] = useState<number>(0);
-	useEffect(() => {
-		const specs = data?.spec ? JSON.parse(data.spec) : [];
-		setMetas([
-			{
-				id: 1,
-				title: 'SPECS',
-				content: specs
-			}
-		])
-	}, [data])
-	const renderContent = (content: Array<{ id: number, spectValue: string }>) => {
-		if (!content || content.length === 0) {
-			return <p></p>;
-		}
-		return (
-			<ul className="list-disc ml-6">
-				{content.map((item, index) => (
-					<li key={index}>{item.spectValue}</li>
-				))}
-			</ul>
-		)
-
-	}
-	return (
-		<>
-			{metas.map((item: any, index: any) => (
-				<Collapse
-					i={index}
-					key={item.title}
-					title={item.title}
-					translatorNS="review"
-					content={
-						metas.length === item.id ? (
-							<>
-								{renderContent(item.content)}
-							</>
-						) : (
-							item.content
-						)
-					}
-					expanded={expanded}
-					setExpanded={setExpanded}
-					variant="transparent"
-				/>
-			)
-			)}
-		</>
-	);
+  const [metas, setMetas] = useState<any[]>([]);
+  const [expanded, setExpanded] = useState<number>(0);
+  useEffect(() => {
+    const specs = data?.spec ? JSON.parse(data.spec) : [];
+    setMetas([
+      {
+        id: 1,
+        title: "SPECS",
+        content: specs,
+      },
+    ]);
+  }, [data]);
+  const renderContent = (content: any) => {
+    if (!content || content.length === 0) {
+      return <p></p>;
+    }
+    return (
+      <ul className="list-disc ml-6">
+        {content.map((item: string, index: number) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    );
+  };
+  return (
+    <>
+      {metas.map((item: any, index: any) => (
+        <Collapse
+          i={index}
+          key={item.title}
+          title={item.title}
+          translatorNS="review"
+          content={
+            metas.length === item.id ? (
+              <>{renderContent(item.content)}</>
+            ) : (
+              item.content
+            )
+          }
+          expanded={expanded}
+          setExpanded={setExpanded}
+          variant="transparent"
+        />
+      ))}
+    </>
+  );
 };
 
 export default ProductMetaReview;
